c36e20bf5ba496240f9048d94a9c56e0a04746b6
緒方誠 <ogatamakoto1976@gmail.com>
2016-08-06 16:29:31 +0900

    add function

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
 var start = document.getElementById('start');
 var message = 'ボタンを押しました';
+var number = document.getElementById('number');
+var count = 0;
+
 
 start.onclick = function(){
-	alertFuncton();
+	countUp();
 }
 
 function alertFuncton (){
 	alert(message);
 }
+
+var countUp = function() {
+	number.innerHTML = count;
+	count++;
+	setTimeout(countUp, 1000);
+}
+
