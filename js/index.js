90a86dd357f8a48c7f2488d5fad0b8f785495a44
緒方誠 <ogatamakoto1976@gmail.com>
2016-08-13 17:06:18 +0900

    fix javascript file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-var action = document.getElementById('action');
 var number = document.getElementById('number');
 var reset = document.getElementById('reset');
 var count = 0;
@@ -6,12 +5,14 @@ var btnText = 0;
 
 action.onclick = function(){
 	if (btnText == 0){
-		$(action).text('ストップ');
+		$('#action').text('ストップ');
+		$('#reset').text('ラップ');
 		btnText = 1;
 		myInterval = setInterval(countUp ,1000);
 	} else {
 		btnText = 0;
-		$(action).text('スタート');
+		$('#action').text('スタート');
+		$('#reset').text('リセット');
 		clearInterval(myInterval);
 	}
 }
